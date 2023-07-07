7c7387d44c050d0db09e69715253625785acc154
PioneerX7 <vita-kopy@yandex.ru>
2023-07-07 19:06:47 +0300

    first

---
+++ src/main/java/ru/skypro/cweasyauction/CwEasyAuctionApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.cweasyauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwEasyAuctionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CwEasyAuctionApplication.class, args);
+	}
+
+}
